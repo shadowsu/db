fa061ae7e1b615f8d2c482a7dab1361e5b1f5a43
shadowsu <qianbingjian@163.com>
2013-05-16 19:17:30 +0800

    Create update.php

---
+++ update.php
@@ -0,0 +1,47 @@
+<?php 
+  include 'conn.php';
+	
+	$domain = isset($_GET['domain'])?$_GET['domain']:null;
+	$name = isset($_GET['name'])?$_GET['name']:null;
+	$nickname = isset($_GET['nickname'])?$_GET['nickname']:null;
+	$description = isset($_GET['description'])?$_GET['description']:null;
+	$ip = isset($_GET['ip'])?$_GET['ip']:null;
+	$ftpaddress = isset($_GET['ftpaddress'])?$_GET['ftpaddress']:null;
+	$ftpusr = isset($_GET['ftpusr'])?$_GET['ftpusr']:null;
+	$ftppwd = isset($_GET['ftppwd'])?$_GET['ftppwd']:null;
+?>  
+
+
+<?php
+	
+		$nn=$_GET['id'];
+		echo $nn;
+		$result = mysql_query('SELECT * FROM `'.$table_name.'` WHERE `id` = \'' . $nn . '\'');
+						
+		while($row = mysql_fetch_array($result)){
+			 echo '<form action="update.php?id='.$nn.'" method="get">
+						域名：<input type="text" name="domain"  id="domain" value="'.$row['domain'].'"/></br>
+						名字：<input type="text" name="name" id="name" value="'. $row['name'].'"/></br>
+						昵称：<input type="text" name="nickname"  id="nickname" value="'.$row['nickname'].'"/></br>
+						备注：<input type="text" name="description" id="description" value="'.$row['description'].'"/></br>
+						ip：<input type="text" name="ip" id="ip" value="'.$row['ip'].'"/></br>
+						ftp地址：<input type="text" name="ftpaddress" id="ftpaddress" value="'.$row['ftpaddress'].'"/></br>
+						ftp用户名：<input type="text" name="ftpusr" id="ftpusr" value="'.$row['ftpusr'].'"/></br>
+						ftp密码：<input type="text" name="ftppwd" id="ftppwd" value="'.$row['ftppwd'].'"/></br>
+						<input type="submit" value="保存修改"  name="submit"/>
+						<a href="db.php">返回</a>';
+						
+			echo '</form>';		
+			
+		}
+		
+		if(isset($_GET['submit'])){
+				$sql="UPDATE `".$table_name."` SET `domain`='".$_GET['domain']."',`name`='".$_GET['name']."',`nickname`='".$_GET['nickname']."',`description`='".$_GET['description']."',`ip`='".$_GET['ip']."',`ftpaddress`='".$_GET['ftpaddress']."',`ftpusr`='".$_GET['ftpusr']."',`ftppwd`='".$_GET['ftppwd']."' WHERE `id`=".$nn."";
+				if(mysql_query($sql,$con))
+				{
+					echo "修改成功";
+				}else{
+					echo "修改不成功";}
+		}
+
+?>
